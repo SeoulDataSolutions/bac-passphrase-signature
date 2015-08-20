06b7c4f5a0c2eab208a929ca4baf7a6945badec8
szabozee <szabozee@github.com>
2015-08-20 20:20:18 +0200

    Adding base crypto functions

--- src/Bac.java
+++ src/Bac.java
@@ -1,7 +1,9 @@
 package bac;
 
+import bac.crypto.Crypto;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
 
 public final class Bac  {
 
@@ -41,6 +43,17 @@ public static final String VERSION = "BAC V1.0";
             long currentTime = System.currentTimeMillis();
             Bac.logMessage("Initialization took " + (currentTime - startTime) / 1000 + " seconds");
             Bac.logMessage("Bac server " + VERSION + " started successfully.");
+            
+            Bac.logMessage("Testing Crypto module");
+
+            byte[] PublicKey = new byte[32];                        
+            PublicKey = Crypto.getPublicKey("secretPhrase");
+            byte[] signature = new byte[64];
+            byte[] message = { 0,1,2,3,4,5,6,7,8,9 };
+            signature = Crypto.sign(message,"secretPhrase");
+            Bac.logMessage("Verify result:"+Boolean.toString(
+            Crypto.verify(signature, message, PublicKey)));
+            
         }
 
         private static void init() {}
